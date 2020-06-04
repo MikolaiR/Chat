9ddcdfc2d155a9c8c2f67d2ea2d373c8920b4d4a
Mikalai Radomski <radomskynn@tut.by>
2020-06-04 20:58:20 +0300

    feat: add firebase storage and implemented a method for sending, saving and loading image

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chat
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,6 +14,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,52 +24,60 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sendMessageButton: Button
     private lateinit var progressBar: ProgressBar
     private lateinit var messageEditText: EditText
-    private lateinit var sendPhotoButton: ImageButton
+    private lateinit var sendImageButton: ImageButton
 
     private lateinit var adapter: AwesomeMessageAdapter
-    private var userName:String? = null
-    private lateinit var awesomeMessage:MutableList<AwesomeMessage>
+    private var userName: String? = null
+    private lateinit var awesomeMessage: MutableList<AwesomeMessage>
 
     private lateinit var database: FirebaseDatabase
     private lateinit var messageDatabaseReference: DatabaseReference
-    private lateinit var messageChildEventListener:ChildEventListener
+    private lateinit var messageChildEventListener: ChildEventListener
     private lateinit var usersDatabaseReference: DatabaseReference
     private lateinit var usersChildEventListener: ChildEventListener
+    private lateinit var storage: FirebaseStorage
+    private lateinit var chatImageStorageReference: StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         database = Firebase.database
+        storage = Firebase.storage
         messageDatabaseReference = database.getReference("message")
         usersDatabaseReference = database.getReference("users")
+        chatImageStorageReference = storage.getReference("chat_images")
 
         sendMessageButton = findViewById(R.id.sendMessageButton)
         progressBar = findViewById(R.id.progressBar)
         messageEditText = findViewById(R.id.messageEditText)
-        sendPhotoButton = findViewById(R.id.sendPhotoButton)
+        sendImageButton = findViewById(R.id.sendImageButton)
         messageListView = findViewById(R.id.messageListView)
 
         val intent = intent
-        if (intent.getStringExtra(USER_NAME) == null){
-            usersChildEventListener =object: ChildEventListener{
+        if (intent.getStringExtra(USER_NAME) == null) {
+            usersChildEventListener = object : ChildEventListener {
                 override fun onCancelled(p0: DatabaseError) {
                 }
+
                 override fun onChildMoved(p0: DataSnapshot, p1: String?) {
                 }
+
                 override fun onChildChanged(p0: DataSnapshot, p1: String?) {
                 }
+
                 override fun onChildAdded(p0: DataSnapshot, p1: String?) {
                     val user = p0.getValue(User::class.java)!!
-                    if (user.id == FirebaseAuth.getInstance().currentUser!!.uid){
+                    if (user.id == FirebaseAuth.getInstance().currentUser!!.uid) {
                         userName = user.name
                     }
                 }
+
                 override fun onChildRemoved(p0: DataSnapshot) {
                 }
             }
             usersDatabaseReference.addChildEventListener(usersChildEventListener)
-        }else{
+        } else {
             userName = intent.getStringExtra(USER_NAME)
         }
 
@@ -92,26 +104,36 @@ class MainActivity : AppCompatActivity() {
 
         messageEditText.filters = arrayOf(InputFilter.LengthFilter(500))
         sendMessageButton.setOnClickListener {
-            val message = AwesomeMessage(messageEditText.text.toString(),userName!!,null)
+            val message = AwesomeMessage(messageEditText.text.toString(), userName!!, null)
             messageDatabaseReference.push().setValue(message)
 
             messageEditText.setText("")
         }
-        sendPhotoButton.setOnClickListener {
-
+        sendImageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = IMAGE_TYPE
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            startActivityForResult(
+                Intent.createChooser(intent, "Choose an song"),
+                REQUEST_CODE_IMAGE
+            )
         }
 
-        messageChildEventListener = object:ChildEventListener{
+        messageChildEventListener = object : ChildEventListener {
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
-               val message = p0.getValue(AwesomeMessage::class.java)
+                val message = p0.getValue(AwesomeMessage::class.java)
                 adapter.add(message)
             }
+
             override fun onChildChanged(p0: DataSnapshot, p1: String?) {
             }
+
             override fun onChildRemoved(p0: DataSnapshot) {
             }
+
             override fun onChildMoved(p0: DataSnapshot, p1: String?) {
             }
+
             override fun onCancelled(p0: DatabaseError) {
             }
         }
@@ -120,15 +142,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu,menu)
+        inflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
-            R.id.sign_out ->{
+        return when (item.itemId) {
+            R.id.sign_out -> {
                 FirebaseAuth.getInstance().signOut()
-                startActivity(Intent(this@MainActivity,SignInActivity::class.java))
+                startActivity(Intent(this@MainActivity, SignInActivity::class.java))
                 true
             }
             else -> {
@@ -136,7 +158,39 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    companion object{
-        const val USER_NAME:String = "userName"
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == Activity.RESULT_OK) {
+            val selectedImageUri = data!!.data
+            val imageReference =
+                chatImageStorageReference.child(selectedImageUri!!.lastPathSegment!!)
+            val uploadTask = imageReference.putFile(selectedImageUri)
+            val urlTask = uploadTask.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                imageReference.downloadUrl
+            }.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    val message = AwesomeMessage()
+                    message.imageUrl = downloadUri.toString()
+                    message.name = userName!!
+                    messageDatabaseReference.push().setValue(message)
+                } else {
+                    // Handle failures
+                    // ...
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val USER_NAME: String = "userName"
+        const val IMAGE_TYPE = "image/jpeg"
+        const val REQUEST_CODE_IMAGE = 100
     }
 }
